df72314a2e6eaa1bfe1d259ffc77ac8a1cc16218
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2025-08-19 14:42:05 +0800

    afd-poc: support stepmesh
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>

--- python/sglang/srt/layers/afd.py
+++ python/sglang/srt/layers/afd.py
@@ -21,6 +21,13 @@ from typing import Any, Dict, Optional, List, Tuple
 from abc import ABC, abstractmethod
 from functools import cache
 
+import sys
+import os
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
 import torch
 from torch import nn
 import torch.distributed as dist
@@ -124,11 +131,213 @@ class ZMQSimpleTensorCommunicator(FifoTensorCommunicator):
         socket = self.get_push_socket()
         socket.send_pyobj(x)
 
+class StepMeshTensorCache(object):
+    def __init__(self):
+        self.push_tensor = None
+        self.pull_tensor = None
+
+        self.push_key = 0
+        self.pull_key = 0
+
+        self.h = None
+
+def stepmesh_scheduler():
+    os.environ['DMLC_ROLE'] = 'scheduler'
+
+    print("StepMesh scheduler: DMLC_PS_ROOT_URI=%s" % os.environ["DMLC_NODE_HOST"])
+    import fserver_lib as f
+
+    f.init()
+    print("StepMesh scheduler init done.")
+
+    while True:
+        time.sleep(10000)
+
+    f.stop()
+
+class StepMeshTensorCommunicator(FifoTensorCommunicator):
+    def __init__(self, afd_perspective: AFDPerspective):
+        self.perspective = afd_perspective
+
+        super().__init__()
+
+        import fserver_lib as f
+
+        self.start_stepmesh_scheduler()
+
+        time.sleep(10) # wait scheduler
+
+        logger.info("%s init..." % os.environ['DMLC_ROLE'])
+        f.init()
+        logger.info("%s init done." % os.environ['DMLC_ROLE'])
+
+        self.worker_num = int(os.environ['DMLC_NUM_WORKER'])
+
+        self.key = 0
+        self.gpu = torch.cuda.current_device()
+        self.tensor_shape = None
+
+        self.f = f
+
+        self.comm_ids = []
+        self.waits = []
+        self.free_tensors = {}
+
+    def env_def(self, env, v):
+        if os.environ.get(env) == None:
+            os.environ[env] = v
+
+    def get_node_ip(self):
+        if os.environ.get("DMLC_NODE_HOST") != None:
+            return
+
+        import psutil
+
+        interface_name = os.environ.get("MLC_INTERFACE")
+
+        interfaces = psutil.net_if_addrs()
+
+        if interface_name not in interfaces:
+            print("Invalid MLC_INTERFACE %s" % interface_name)
+            return
+
+        for addr in interfaces[interface_name]:
+            if addr.family == 2:  # socket.AF_INET
+                os.environ["DMLC_NODE_HOST"] = addr.address
+                break
+
+    def start_stepmesh_scheduler(self):
+        self.get_node_ip()
+
+        gpu = '%s' % torch.cuda.current_device()
+
+        self.env_def('DMLC_NODE_RANK',    '0')
+        self.env_def('DMLC_NUM_SERVER',   '1')
+        self.env_def('DMLC_NUM_WORKER',   '1')
+        self.env_def('DMLC_GROUP_SIZE',   '1')
+        self.env_def('DMLC_PS_ROOT_PORT', '8123')
+        self.env_def('DMLC_ENABLE_RDMA',  'ibverbs')
+        self.env_def('STEPMESH_GPU',      gpu)
+
+        if afd_is_attn():
+            os.environ['DMLC_ROLE'] = 'worker'
+        else:
+            os.environ['DMLC_ROLE'] = 'server'
+
+        if os.environ['DMLC_ROLE'] != 'worker':
+            return
+
+        if os.environ.get('DMLC_NODE_RANK') != '0':
+            return
+
+        if os.environ['STEPMESH_GPU'] != '0':
+            return
+
+        if os.environ.get('STEPMESH_SCHEDULER_STARTED') == '1':
+            return
+
+        os.environ['STEPMESH_SCHEDULER_STARTED'] = '1'
+        os.environ["DMLC_PS_ROOT_URI"] = os.environ["DMLC_NODE_HOST"]
+
+        import multiprocessing
+
+        p = multiprocessing.Process(target=stepmesh_scheduler)
+        p.daemon = True
+        p.start()
+
+    def attn_send(self, x):
+        free = self.free_tensors.get(x.shape)
+        if free == None:
+            self.free_tensors[x.shape] = []
+            free = self.free_tensors[x.shape]
+
+        if len(free) < 100:
+            self.key += 2
+
+            t = StepMeshTensorCache()
+
+            t.push_tensor = torch.empty_like(x)
+            t.pull_tensor = torch.empty_like(x)
+            t.pull_tensor.zero_()
+
+            t.push_key = self.key
+            t.pull_key = self.key + 1
+
+        else:
+            t = free.pop(0)
+
+        t.push_tensor.copy_(x)
+
+        h = self.f.push_pull(
+                [t.push_tensor],
+                [t.push_key],
+                [t.pull_tensor],
+                [t.pull_key])
+
+        t.h = h
+
+        self.waits.append(t)
+
+    def attn_recv(self):
+        t = self.waits.pop(0)
+        self.f.wait(t.h)
+
+        self.free_tensors[t.push_tensor.shape].append(t)
+
+        return t.pull_tensor.clone()
+
+    def ffn_send(self, x):
+        free = self.free_tensors.get(x.shape)
+        if free == None:
+            self.free_tensors[x.shape] = []
+            free = self.free_tensors[x.shape]
+
+        if len(free) < 100:
+            t = torch.empty_like(x)
+        else:
+            t = free.pop(0)
+
+        t.copy_(x)
+
+        c = self.comm_ids.pop(0)
+        self.f.respond([t], c, True)
+
+        free.append(t)
+
+    def ffn_recv(self):
+        batches = self.f.get_batch()
+
+        ## batches [
+        #     [comm_id, push_tensor_list, key_list],
+        #     [comm_id, push_tensor_list, key_list],
+        # ]
+        assert len(batches) == 1, "just handle for one worker"
+
+        x = batches[0][1][0]
+        self.comm_ids.append(batches[0][0])
+
+        return x.clone()
+
+    def recv_tensor(self) -> torch.Tensor:
+        if self.perspective == AFDPerspective.AFD_PERSPECTIVE_ATTN:
+            return self.attn_recv()
+        else:
+            return self.ffn_recv()
+
+    def send_tensor(self, x: torch.Tensor):
+        if self.perspective == AFDPerspective.AFD_PERSPECTIVE_ATTN:
+            self.attn_send(x)
+        else:
+            self.ffn_send(x)
+
 @cache
 def get_tensor_communicator() -> FifoTensorCommunicator:
     afd_perspective = get_afd_perspective()
     if afd_perspective is not None:
-        return ZMQSimpleTensorCommunicator(afd_perspective)
+        if os.environ.get("MLC_INTERFACE"):
+            return StepMeshTensorCommunicator(afd_perspective)
+        else:
+            return ZMQSimpleTensorCommunicator(afd_perspective)
     else:
         raise NotImplementedError
 
